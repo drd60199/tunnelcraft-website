55f56d880681e95acf0c7e108b63b92e131c25e3
Damien Dennis <dennis.damien95@outlook.com>
2025-08-25 05:17:29 -0500

    feat: implement firebase login system

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+session_start(); // Access the existing session
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session cookie and the session itself
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+session_destroy();
+
+// Redirect to the homepage
+header("Location: index.php");
+exit;
+?>
